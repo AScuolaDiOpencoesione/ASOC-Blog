90bb1ed5d5d8855e771d77c2f4d3760373e4e6d7
Marco Montanari <sirmmo@gmail.com>
2016-11-22 07:27:06 +0000

    gh

--- asoc-blogs.php
+++ asoc-blogs.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              http://ingmmo.com
- * @since             1.0.0
+ * @since             1.0.2
  * @package           Asoc_Blogs
  *
  * @wordpress-plugin
