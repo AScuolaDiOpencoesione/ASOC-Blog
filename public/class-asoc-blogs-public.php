f52c6793fe5bb02e7a20bf1471b27e83ed4e731e
Marco Montanari <sirmmo@gmail.com>
2016-12-09 12:28:10 +0000

    msg

--- public/class-asoc-blogs-public.php
+++ public/class-asoc-blogs-public.php
@@ -398,6 +398,11 @@ class Asoc_Blogs_Public {
 			if ($wp->query_vars["asoc_mode"] == "blog"){
 				//LATEST POSTS
 				echo "<h3>Gli ultimi post</h3>";
+				$latest = file_get_contents('http://'.$testsrvr.'api.ascuoladiopencoesione.it/meta/compiledform/latest');
+				$latest = json_decode($latest);
+				
+				
+				
 			} else {
 				//TEAM PROFILE
 				echo "<center>";
