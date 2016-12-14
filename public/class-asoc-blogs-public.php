3af2f13922e1f4b04b8742813a2943d831f92765
Marco Montanari <sirmmo@gmail.com>
2016-12-14 06:45:53 +0000

    data

--- public/class-asoc-blogs-public.php
+++ public/class-asoc-blogs-public.php
@@ -300,7 +300,7 @@ class Asoc_Blogs_Public {
 						function onEachFeature(feat) {
 							return function(){
 								let f = feat.properties;
-								return $(".school_"+f.id).clone();
+								return $(".school_"+f.school.id).clone().html();
 							}
 					    }
 				';
